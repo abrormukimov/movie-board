b708eb14485ae4a634f3b88311780a95a092a82d
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-20 14:34:33 +0100

    position element and popup

--- src/index.js
+++ src/index.js
@@ -16,3 +16,76 @@ const uList = elementGenerator('ul');
 const listOne = elementGenerator('li', 'meals');
 const linkOne = elementGenerator('a');
 linkOne.href = '#';
+
+const listTwo = elementGenerator('li');
+const linkTwo = elementGenerator('a');
+linkTwo.href = '#';
+linkTwo.textContent = 'non-Vegetarian';
+
+const listThree = elementGenerator('li');
+const linkThree = elementGenerator('a');
+linkThree.href = '#';
+linkThree.textContent = 'fried';
+
+const footer = elementGenerator('footer');
+footer.textContent = 'Created By Tufoin & Abror under CC licence';
+
+const allmain = document.getElementById('allmain');
+
+const main = elementGenerator('main');
+
+const createPopup = (meal) => {
+  const popupSection = elementGenerator('section', 'popup-window invisible');
+  const popupMarkup = ` 
+    <small class='close-menu'>X</small>   
+    <div class='blur-background'> 
+
+    <div class="popup-img-div"><img class="meal-popup-img" src="${meal.image}" alt="meal" /></div>
+    <div class="popup-details-div">
+    <h1> ${meal.title} </h1>
+    <div class="details-list">
+        <ul>
+        <li>Category: Rice</li>
+        <li>Category: Rice</li>
+        </ul>
+        <ul>
+        <li>Category: Rice</li>
+        <li>Category: Rice</li>
+        </ul>
+    </div>
+  </div>
+  <div class="comments-container">
+<div class="comments-div">
+  <h3>Comments (3)</h3>
+</div>
+<h3>Add a comment</h3>
+<form>
+  <div class="input-group">
+    <input type="text" class="nameInput" placeholder="Your name" />
+  </div>
+  <div class="input-group">
+    <textarea
+      name=""
+      id="commentInput"
+      cols="30"
+      rows="10"
+      placeholder="Your insights"
+    ></textarea>
+  </div>
+  <div class=''button-div>
+  <button type='submit' class='submit-comment-btn'> Comment </button>
+  </div>
+</form>
+</div>
+`;
+popupSection.innerHTML = popupMarkup;
+popupSection.style.display = 'block';
+main.style.display = 'none';
+document.body.appendChild(popupSection);
+const closePopup = document.querySelector('.close-menu');
+closePopup.addEventListener('click', () => {
+  popupSection.remove();
+  main.style.display = 'grid';
+});
+};
+
