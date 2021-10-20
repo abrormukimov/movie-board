8c21533fc4099e4fa96b9ea1d66bef77c360b678
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-20 14:15:39 +0100

    generate header

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import fetchMeals from './myapi.js';
 import icon from './icon.svg';
 import logoImage from './logo.svg';
 import Like from './Like.svg';
+import elementGenerator from './generate.js';
 import { postLikes, getLikes } from './likeFunctions';
+
+const header = elementGenerator('header');
+const logo = elementGenerator('img', 'logo');
+logo.src = logoImage;
+logo.alt = 'restaurant-logo';
+const navigation = elementGenerator('nav');
+const uList = elementGenerator('ul');
+const listOne = elementGenerator('li', 'meals');
+const linkOne = elementGenerator('a');
+linkOne.href = '#';
