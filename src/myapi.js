102a4ac839ff6894c56615b308ca59877f220ed6
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-21 09:16:18 +0100

    add icons

--- src/myapi.js
+++ src/myapi.js
@@ -1,5 +1,5 @@
 const fetchMeals = async () => {
-  const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Canadian');
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
   return response.json();
 };
 
