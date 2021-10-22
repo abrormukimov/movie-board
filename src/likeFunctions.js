b912de1763213b6f81f67e8256827d97f2526354
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-22 20:58:46 +0100

    add get like from api

--- src/likeFunctions.js
+++ src/likeFunctions.js
@@ -17,9 +17,9 @@ export async function postLikes(url = '', data = {}) {
 export async function getLikes(url = '') {
   const response = await fetch(url, {
     method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    // mode: 'cors',
+    // cache: 'no-cache',/
+    // credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
     },
